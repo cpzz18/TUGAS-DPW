18aeb6faab9c83e49da05a98faf6e25ee275f3bc
cpzz18 <robbincfa@gmail.com>
2026-04-06 01:52:08 +0700

    update about.html, contact.html and script.js

--- praktikum 6/script.js
+++ praktikum 6/script.js
@@ -1,5 +1,4 @@
-// Active link highlighting based on current page (optional)
-// Since this is multi-page, we can just add active class based on current URL
+
 document.addEventListener("DOMContentLoaded", function() {
     const currentPage = window.location.pathname.split("/").pop();
     const navLinks = document.querySelectorAll(".navbar-nav .nav-link");
