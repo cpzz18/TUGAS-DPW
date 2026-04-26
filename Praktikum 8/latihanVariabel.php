8b9a4455bad9ba302aebb07732d2c0119551c7c4
cpzz18 <robbincfa@gmail.com>
2026-04-26 22:57:49 +0700

    add latihan variabel

---
+++ Praktikum 8/latihanVariabel.php
@@ -0,0 +1,18 @@
+<?php
+// variabel
+$hari_1 = "Senin";
+$hari_2 = "Selasa"; 
+$hari_3 = "Rabu";
+$hari_4 = "Kamis";
+$hari_5 = "Jumat";  
+$hari_6 = "Sabtu";  
+$hari_7 = "Minggu";
+
+// menampilkan variabel
+echo "Hari 1: $hari_1 <br>";
+echo "Hari 2: $hari_2 <br>";
+echo "Hari 3: $hari_3 <br>";
+echo "Hari 4: $hari_4 <br>";
+echo "Hari 5: $hari_5 <br>";
+echo "Hari 6: $hari_6 <br>";
+echo "Hari 7: $hari_7 <br>";
