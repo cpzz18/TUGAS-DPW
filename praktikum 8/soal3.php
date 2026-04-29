28e4edbffcd5ec018dbb106adffaf3cafe0e8b7c
cpzz18 <robbincfa@gmail.com>
2026-04-29 20:02:37 +0700

    created soal 3

---
+++ praktikum 8/soal3.php
@@ -0,0 +1,14 @@
+<?php
+$siswa = [
+    1 => ["poin" => 75, "siswa" => "adi"],
+    2 => ["poin" => 80, "siswa" => "Joni"],
+    3 => ["poin" => 65, "siswa" => "Jihan"],
+    4 => ["poin" => 70, "siswa" => "Aya"],
+    5 => ["poin" => 85, "siswa" => "Ita"],
+    6 => ["poin" => 90, "siswa" => "Budi"],
+    7 => ["poin" => 95, "siswa" => "Tini"],
+    8 => ["poin" => 65, "siswa" => "Sari"],
+];
+
+echo "a) Tampilkan poin siswa dengan nomor urut 5  <br>";
+echo "Poin: " . $siswa[1]["poin"];
