faef13c3cc956f8e8afaa049869df72b48dd6ce6
cpzz18 <robbincfa@gmail.com>
2026-05-05 22:44:34 +0700

    addd form komentar

--- praktikum 9/form_komentar.php
+++ praktikum 9/form_komentar.php
@@ -5,25 +5,36 @@
 </head>
 <body>
     <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
-        Nama: <input type="text" name="nama"><br>
+        Nama: <input type="text" name="name"><br>
         Email: <input type="email" name="email"><br>
-        Komentar: <textarea name="komentar"></textarea><br>
+        Komentar: <textarea name="comment" rows="5" cols="40"></textarea><br>
         <input type="submit" value="Kirim"><br>
+        <input type="reset" value="Bersihkan"><br>
     </form>
 
 <?php
 
 $name = $email = $comment = "";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['nama'];
+    $name = $_POST['name'];
     $email = $_POST['email'];
-    $comment = $_POST['komentar'];
+    $comment = $_POST['comment'];
 
-    echo "<h2>Data Komentar</h2>";
-    echo "Nama: " . htmlspecialchars($name) . "<br>";
-    echo "Email: " . htmlspecialchars($email) . "<br>";
-    echo "Komentar: " . nl2br(htmlspecialchars($comment)) . "<br>";
+    echo("Nama: " . $name . "<br>");
+    echo("Email: " . $email . "<br>");
+    echo("Komentar: " . $comment . "<br>");
+    echo("<hr>");
+
+    //kesimpulan output
+    echo "Menampilkan data yang dimasukkan oleh pengguna";
 }
+function bersihkan_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
 ?>
 </body>
 </html>
